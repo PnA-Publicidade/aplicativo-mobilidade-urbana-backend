b7b9f6d1a739c55ebc8cd115fa92ebd19ef6efaa
diogo-guimaraes <diogoguimaraes.br@hotmail.com>
2026-09-16 11:15:02 -0400

    add BannerPublicidade

---
+++ app/Http/Controllers/BannerPublicidadeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBannerPublicidadeRequest;
+use App\Http\Requests\UpdateBannerPublicidadeRequest;
+use App\Models\BannerPublicidade;
+
+class BannerPublicidadeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBannerPublicidadeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BannerPublicidade $bannerPublicidade)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBannerPublicidadeRequest $request, BannerPublicidade $bannerPublicidade)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BannerPublicidade $bannerPublicidade)
+    {
+        //
+    }
+}
